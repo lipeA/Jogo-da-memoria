2b0343ad45692a021dce17ac02de3883b4fd6c37
FELIPE ASSIS DE JESUS <45512256+lipeA@users.noreply.github.com>
2021-10-06 14:43:59 -0300

    Finalizando o jogo
    Foi adicionado as ultimas funções em javascript.

--- script.js
+++ script.js
@@ -38,3 +38,94 @@ let lightColor = (element, number) =>{
         element.classList.remove('selected');
     })
 }
+
+//verificando  as cores.
+let checkOrder = () =>{
+    for(let i in clickedOrder){
+        if(clickedOrder[i] != order[i]){
+            gameover();
+            break;
+        }
+    }
+    if(clickedOrder.length == order.length){
+        alert(` Pontuação: ${score}\n você ACERTOU  Iniciando o proximo nivel!`)
+        nextLevel();
+    }
+}
+
+
+//capturando o click.
+
+let click = (color) =>{
+    clickedOrder[clickedOrder.length] = color;
+    createElement(color).classList.add('selected');
+
+    setTimeout(() => {
+        createElement(color).classList.remove('selected');
+        checkOrder();
+    }, 250);
+   
+}
+
+
+// 0 = verde
+// 1 = vermelho
+// 2 = amarelo
+// 3 = azul
+//função que retorna a cor
+ let createElement = (color) =>{
+     if(color == 0){
+        return corVerde;
+     }else if(color == 1){
+         return corVermelho
+     }else if(color == 2){
+         return corAmarelo
+     }else if(color == 3){
+         return corAzul 
+     }
+ }
+
+
+ //função para o proximo level
+ let nextLevel = () =>{
+     score ++;
+     shuffOrder();
+ }
+
+ //funçao para gameover
+let gameover = () =>{
+    alert(`Pontuação: ${score}! \n voce perdeu o jogo\n click em ok para reiniciar o jogo`);
+    order = [];
+    clickedOrder = [];
+    playgame();
+}
+
+
+//inicio do jogo
+let playgame = () =>{
+    alert('Bem vindo ao jogo Gênesis')
+    score = 0;
+
+    nextLevel();
+}
+
+// corVerde.addEventListener('click', click(0));
+// corVerde.addEventListener('click', click(1));
+// corAmarelo.addEventListener('click', click(2));
+// corAzul.addEventListener('click', click(3));
+// 0 = verde
+// 1 = vermelho
+// 2 = amarelo
+// 3 = azul
+
+//evento de click
+corVerde.onclick = () => click(0);
+
+corVermelho.onclick = () => click(1);
+
+corAmarelo.onclick = () => click(2);
+
+corAzul.onclick = () => click(3);
+
+
+playgame();
